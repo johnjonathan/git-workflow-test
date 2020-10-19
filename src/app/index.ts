913c894fbb3bbfc9716bf84fbfacc88131330c8d
John Silva <john.silva@simplustec.com.br>
2020-10-19 00:57:23 -0300

    feature: PR test

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,8 +1,15 @@
 export const entrypoint = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Ok, we`re good",
-    }),
-  };
-};
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Ok, we`re good',
+        }),
+    }
+}
+
+export const doRedirect = async () => {
+    return {
+        statusCode: 301,
+        location: 'github.com',
+    }
+}
