c6fe1a9525c6dcc3db6a881eb29de0392f20ad9d
John Silva <john.silva@simplustec.com.br>
2020-10-19 00:05:13 -0300

    feature: add jest-extended and a new test for index.ts

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,16 @@
-const { compilerOptions } = require("./tsconfig.json");
-const { pathsToModuleNameMapper } = require("ts-jest/utils");
+const { compilerOptions } = require('./tsconfig.json')
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
 
 module.exports = {
-  clearMocks: true,
-  coveragePathIgnorePatterns: ["/node_modules/"],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>",
-  }),
-  preset: "ts-jest",
-  setupFiles: ["dotenv/config"],
-  setupFilesAfterEnv: ["jest-extended"],
-  testEnvironment: "node",
-};
+    clearMocks: true,
+    coveragePathIgnorePatterns: ['/node_modules/'],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+        prefix: '<rootDir>',
+    }),
+    preset: 'ts-jest',
+    setupFiles: ['dotenv/config'],
+    setupFilesAfterEnv: ['jest-extended'],
+    testEnvironment: 'node',
+}
 
 // jest.setTimeout(15000);
